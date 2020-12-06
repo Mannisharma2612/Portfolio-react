02d4b5c1e7f358995608c459840fbc65feb9b90e
Manni Sharma <mannisharma1226@gmail.com>
2020-12-06 13:48:57 +0530

    Custom drawer navigation

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import MainTabScreen from './screens/MainTabScreen';
+import { DrawerContent } from './screens/DrawerContent';
+import SupportScreen from './screens/SupportScreen';
+import SettingScreen from './screens/SettingScreen';
+import BookMarkScreen from './screens/BookMarkScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+const Drawer = createDrawerNavigator();
+
+const App = () => {
+  return(
+    <NavigationContainer>
+        <Drawer.Navigator drawerContent={props => <DrawerContent {... props} />}>
+          <Drawer.Screen name="HomeDrawer" component={MainTabScreen} />
+          <Drawer.Screen name="SupportScreen" component={SupportScreen} />
+          <Drawer.Screen name="SettingScreen" component={SettingScreen} />
+          <Drawer.Screen name="BookMarkScreen" component={BookMarkScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
