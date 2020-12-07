032ca37ee2fc32d4bc2327cd8996791e37d1fc4b
Manni Sharma <mannisharma1226@gmail.com>
2020-12-07 13:56:19 +0530

    SignIn and SignUp

--- App.js
+++ App.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+
 import MainTabScreen from './screens/MainTabScreen';
 import { DrawerContent } from './screens/DrawerContent';
 import SupportScreen from './screens/SupportScreen';
 import SettingScreen from './screens/SettingScreen';
 import BookMarkScreen from './screens/BookMarkScreen';
+import RootStack from './screens/RootStackScreen';
+import RootStackScreen from './screens/RootStackScreen';
+
 
 const Drawer = createDrawerNavigator();
 
 const App = () => {
   return(
     <NavigationContainer>
-        <Drawer.Navigator drawerContent={props => <DrawerContent {... props} />}>
+      <RootStackScreen/>
+        {/* <Drawer.Navigator drawerContent={props => <DrawerContent {... props} />}>
           <Drawer.Screen name="HomeDrawer" component={MainTabScreen} />
           <Drawer.Screen name="SupportScreen" component={SupportScreen} />
           <Drawer.Screen name="SettingScreen" component={SettingScreen} />
           <Drawer.Screen name="BookMarkScreen" component={BookMarkScreen} />
-      </Drawer.Navigator>
+      </Drawer.Navigator> */}
     </NavigationContainer>
   );
 }
